7a7649ec9ddc10563b679fb5ed7659625c826514
M. Edgar Joel <mkakajoel@gmail.com>
2016-04-25 20:35:41 +0000

    Refactored code. Added functionality

--- lesson2/mortgage_calculator.rb
+++ lesson2/mortgage_calculator.rb
@@ -1,88 +1,82 @@
-# 1. ask user to submit loan amount, APR, and loan duration
-    # i) validate user inputs
-# 2. calculate monthly interest rate
-# 3. calculate load duration in months
 require 'yaml'
 require './helper_methods.rb'
 
 MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
-LANGUAGE = 'en'
+LANGUAGE = 'en'.freeze
 MONTHS_IN_YEAR = 12
 
 def validate_apr(apr)
-    apr = apr.split('%')[0] # In case input has '%' sign, strip it
-    number?(apr)
+  apr = apr.split('%')[0] # In case input has '%' sign, strip it
+  number?(apr)
 end
 
 def validate_loan(loan)
-    number?(loan)
+  number?(loan)
 end
 
 def validate_duration(years)
-    number?(years)
+  number?(years)
 end
 
 def loan_months(years)
-    years * MONTHS_IN_YEAR
+  years * MONTHS_IN_YEAR
 end
 
 def monthly_interest_rate(apr)
-    apr.to_f / (MONTHS_IN_YEAR * 100)
+  apr.to_f / (MONTHS_IN_YEAR * 100)
 end
 
-def get_loan
+def loan_amount
   prompt('loan')
   loan = gets.chomp
   loan = validate_loan(loan) #####
-  
+
   return loan if loan != false
   prompt('invalid_loan')
-  get_loan
+  loan_amount
 end
 
-def get_duration
+def duration
   prompt('duration')
   years = gets.chomp
   years = validate_duration(years)
-  
+
   return years if years != false
   prompt('invalid_duration')
-  get_duration
+  duration
 end
 
-def get_apr
+def loan_apr
   prompt('apr')
-  apr= gets.chomp
+  apr = gets.chomp
   apr = validate_apr(apr)
-  
+
   return apr if apr != false
   prompt('invalid_apr')
-  get_apr
+  loan_apr
+end
+
+def monthly_payment
+  loan = loan_amount
+  months = loan_months(duration)
+  rate = monthly_interest_rate(loan_apr)
+  payment = loan * (rate * (1 + rate)**months) / ((1 + rate)**months - 1)
+  payment.round(2)
 end
 
 def run_calculator
-    prompt('welcome')
-    loan = get_loan
-    months = loan_months(get_duration)
-    rate = monthly_interest_rate(get_apr)
-    monthly_payment = loan * (rate * (1 + rate)**months ) / ((1 + rate)**months - 1)
-    puts "Your monthly payments are: $#{monthly_payment.round(2)} per month}"
+  prompt('welcome')
+  payment = monthly_payment
+  puts "Your monthly payments are: $#{payment} per month}"
+
+  prompt('new')
+  decision = gets.chomp
+
+  if decision.casecmp('Y') == 0
+    run_calculator
+  else
+    prompt('exit')
+  end
 end
 
 run_calculator
-
-    
-    
-    
-    
-#fixed_monthly_payment = loan(monthly_interest_rate(1 + monthly_interest_rate)**months) / ((1 + monthly_interest_rate)**months - 1)
-
-
-
-
-
-
-
-
-      
-      
